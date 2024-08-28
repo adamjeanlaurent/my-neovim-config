232807fa69e4b7253401df280c093a7ee656ac4d
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2024-08-28 15:06:52 -0400

    adjust gitlens date and format

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -10,8 +10,8 @@ return {
     -- your configuration comes here
     -- for example
     enabled = true, -- if you want to enable the plugin
-    message_template = ' <summary> • <date> • <author> • <<sha>>', -- template for the blame message, check the Message template section for more options
-    date_format = '%m-%d-%Y %H:%M:%S', -- template for the date, check Date format section for more options
+    message_template = '<<sha>> • <author> • <date>', -- template for the blame message, check the Message template section for more options
+    date_format = '%m-%Y', -- template for the date, check Date format section for more options
     virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
   },
 }
