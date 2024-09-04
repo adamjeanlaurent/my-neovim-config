80ecf50a7444c03ccf3f95c31f810389f79633e4
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2024-09-04 08:43:24 -0400

    telescope truncate paths

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,11 +1,28 @@
+local function close_neo_tree()
+  require('neo-tree.sources.manager').close_all()
+  vim.notify 'closed all'
+end
+
+local function open_neo_tree()
+  vim.notify 'opening neotree'
+  require('neo-tree.sources.manager').show 'filesystem'
+end
+
 return {
   {
     'rmagatti/auto-session',
     lazy = false,
     config = function()
-      require('auto-session').setup {}
+      require('auto-session').setup {
+        bypass_session_save_file_types = { 'neo-tree' },
+        pre_save_cmds = {
+          close_neo_tree,
+        },
+        post_restore_cmds = {
+          open_neo_tree,
+        },
+      }
     end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
-
