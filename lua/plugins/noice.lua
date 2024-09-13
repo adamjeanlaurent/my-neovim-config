f05068ee8b5b101db4e02d505e61637b782b0038
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2024-09-13 18:04:12 -0400

    noice ignore file save

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,8 +11,22 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
+      -- 'rcarriga/nvim-notify',
     },
+    config = function()
+      require('noice').setup {
+        routes = {
+          {
+            filter = {
+              event = 'msg_show',
+              kind = '',
+              find = 'written',
+            },
+            opts = { skip = true },
+          },
+        },
+      }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
