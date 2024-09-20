6359de488c5f4c039ffe80ab6b564cd14daa6f7c
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2024-09-20 19:47:11 -0400

    idk

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -7,36 +7,32 @@ return {
         -- Add any custom configurations here if needed.
       }
 
-      -- use catppuccin at work
-      local hostname = io.popen('hostname'):read '*l'
-      if hostname ~= 'Adams-MBP' then
-        vim.cmd.colorscheme 'catppuccin'
-      end
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
-  {
-    'asilvam133/rose-pine.nvim',
-    priority = 1000, -- Ensure this is loaded before other plugins.
-    config = function()
-      require('rose-pine').setup {
-        styles = {
-          bold = true,
-          italic = false,
-        },
-        highlight_groups = {
-          ['@function.builtin.lua'] = { italic = false },
-          ['@lsp.type.comment'] = { italic = true },
-          ['@lsp.typemod.function.defaultLibrary.lua'] = { italic = false },
-          Comment = { italic = true },
-          DiagnosticUnnecessary = { italic = false },
-        },
-      }
-      local hostname = io.popen('hostname'):read '*l'
-
-      -- use rose pine for personal use
-      if hostname == 'Adams-MBP' then
-        vim.cmd.colorscheme 'rose-pine'
-      end
-    end,
-  },
+  -- {
+  --   'asilvam133/rose-pine.nvim',
+  --   priority = 1000, -- Ensure this is loaded before other plugins.
+  --   config = function()
+  --     require('rose-pine').setup {
+  --       styles = {
+  --         bold = true,
+  --         italic = false,
+  --       },
+  --       highlight_groups = {
+  --         ['@function.builtin.lua'] = { italic = false },
+  --         ['@lsp.type.comment'] = { italic = true },
+  --         ['@lsp.typemod.function.defaultLibrary.lua'] = { italic = false },
+  --         Comment = { italic = true },
+  --         DiagnosticUnnecessary = { italic = false },
+  --       },
+  --     }
+  --     local hostname = io.popen('hostname'):read '*l'
+  --
+  --     -- use rose pine for personal use
+  --     if hostname == 'Adams-MBP' then
+  --       vim.cmd.colorscheme 'rose-pine'
+  --     end
+  --   end,
+  -- },
 }
