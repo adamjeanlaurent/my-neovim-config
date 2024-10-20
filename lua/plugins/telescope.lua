3ac94c47cb1c483c46f0dfc598e720771d4cb012
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2024-10-20 19:51:24 -0400

    telescope jumplist

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -55,6 +55,7 @@ return {
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sj', builtin.jumplist, { desc = '[S]earch jumplist' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
