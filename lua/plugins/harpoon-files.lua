be02fe26b64d4a02094e82612fce16562db4cd2a
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2024-12-22 23:16:34 -0500

    lualine updates for harpoon

---
+++ lua/plugins/harpoon-files.lua
@@ -0,0 +1,17 @@
+return {
+  'kiennt63/harpoon-files.nvim',
+  dependencies = {
+    { 'ThePrimeagen/harpoon', branch = 'harpoon2' },
+  },
+  opts = {
+    max_length = 10,
+    icon = '',
+    show_icon = true,
+    show_index = true,
+    show_filename = true,
+    separator_left = ' ',
+    separator_right = ' ',
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,39 +2,20 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', path = 1 } },
-        lualine_x = {
-          {
-            'harpoon2',
-            indicators = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' },
-            active_indicators = { '[1]', '[2]', '[3]', '[4]', '[5]', '[6]', '[7]', '[8]', '[9]', '[10]' },
-          },
+    config = function()
+      local harpoon_files = require 'harpoon_files'
+      require('lualine').setup {
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { { 'filename', path = 1 } },
+          lualine_x = { { harpoon_files.lualine_component } },
+          lualine_y = {},
+          lualine_z = {},
         },
-        lualine_y = { 'location', 'filetype' },
-        lualine_z = {},
-      },
-      inactive_sections = {},
-      -- winbar = {
-      --   lualine_a = {},
-      --   lualine_b = {},
-      --   lualine_c = { { 'filename', path = 1 } },
-      --   lualine_x = {},
-      --   lualine_y = {},
-      --   lualine_z = {},
-      -- },
-      -- inactive_winbar = {
-      --   lualine_a = {},
-      --   lualine_b = {},
-      --   lualine_c = { { 'filename', path = 1 } },
-      --   lualine_x = {},
-      --   lualine_y = {},
-      --   lualine_z = {},
-      -- },
-    },
+        inactive_sections = {},
+      }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
