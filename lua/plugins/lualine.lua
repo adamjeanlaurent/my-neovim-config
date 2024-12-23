2bb00c290006ad48519fd7a2e14454087da6f57d
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2024-12-23 17:45:17 -0500

    Revert "lualine updates for harpoon"
    This reverts commit be02fe26b64d4a02094e82612fce16562db4cd2a.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,20 +2,39 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      local harpoon_files = require 'harpoon_files'
-      require('lualine').setup {
-        sections = {
-          lualine_a = { 'mode' },
-          lualine_b = { 'branch', 'diff', 'diagnostics' },
-          lualine_c = { { 'filename', path = 1 } },
-          lualine_x = { { harpoon_files.lualine_component } },
-          lualine_y = {},
-          lualine_z = {},
+    opts = {
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = {
+          {
+            'harpoon2',
+            indicators = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' },
+            active_indicators = { '[1]', '[2]', '[3]', '[4]', '[5]', '[6]', '[7]', '[8]', '[9]', '[10]' },
+          },
         },
-        inactive_sections = {},
-      }
-    end,
+        lualine_y = { 'location', 'filetype' },
+        lualine_z = {},
+      },
+      inactive_sections = {},
+      -- winbar = {
+      --   lualine_a = {},
+      --   lualine_b = {},
+      --   lualine_c = { { 'filename', path = 1 } },
+      --   lualine_x = {},
+      --   lualine_y = {},
+      --   lualine_z = {},
+      -- },
+      -- inactive_winbar = {
+      --   lualine_a = {},
+      --   lualine_b = {},
+      --   lualine_c = { { 'filename', path = 1 } },
+      --   lualine_x = {},
+      --   lualine_y = {},
+      --   lualine_z = {},
+      -- },
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
