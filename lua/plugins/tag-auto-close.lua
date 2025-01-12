17ccaff804560e8c8fbe1de78e659d019200e9d9
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2025-01-12 18:14:43 -0500

    fix auto close tags

--- lua/plugins/tag-auto-close.lua
+++ lua/plugins/tag-auto-close.lua
@@ -1,12 +1,14 @@
 return {
   {
     'windwp/nvim-ts-autotag',
-    opts = {
-      -- Defaults
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = false, -- Auto close on trailing </
-    },
+    config = function()
+      require('nvim-ts-autotag').setup {
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+      }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
