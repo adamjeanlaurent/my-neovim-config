5d9df3298b365fdc00d41b1d3d1092ee071cff86
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2025-01-29 11:27:35 -0500

    python lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -127,7 +127,7 @@ return {
           end,
         },
 
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
