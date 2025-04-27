e39dc662de55b0a2cd559aa69f49cc5658539690
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2025-04-27 16:01:11 -0400

    add more code browser studd

--- lua/plugins/codebrowser.lua
+++ lua/plugins/codebrowser.lua
@@ -5,8 +5,23 @@ return {
       require('codebrowser').setup {
         urls = {
           {
-            description = 'SourceGraph',
+            description = 'SG: Microrepo',
             base_url = 'https://sg.uberinternal.com/code.uber.internal/uber-code/infra-statsdex/-/blob/',
+            line_number_anchor = '#L', -- sg line anchor
+          },
+          {
+            description = 'SG: Monorepo',
+            base_url = 'https://sg.uberinternal.com/code.uber.internal/uber-code/go-code/-/blob/',
+            line_number_anchor = '#L', -- sg line anchor
+          },
+          {
+            description = 'SG: R2 Rules',
+            base_url = 'https://sg.uberinternal.com/code.uber.internal/uber-code/infra-r2-config/-/blob/',
+            line_number_anchor = '#L', -- sg line anchor
+          },
+          {
+            description = 'SG: Net',
+            base_url = 'https://sg.uberinternal.com/code.uber.internal/uber-code/infra-config/-/blob/',
             line_number_anchor = '#L', -- sg line anchor
           },
         },
